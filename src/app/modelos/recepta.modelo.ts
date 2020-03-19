fbf4f3b110cf3b1227423d5fc00a4e0640de3732
JPGabarre <50842059+JPGabarre@users.noreply.github.com>
2020-03-19 19:08:38 +0100

    Part2

--- src/app/modelos/recepta.modelo.ts
+++ src/app/modelos/recepta.modelo.ts
@@ -1,30 +1,38 @@
 import { Comentari } from './comentari.modelo';
 
+
 export class Recepta {
     public id: number;
     public nombre: string;
     public pasos: string;
     public ingredients: string;
-    public comentaris: Comentari[];
+    public comentaris: Comentari[] = [];
+    //Variable booelana que utilitzaré per saber si es vol editar una recepta o no.
+    public edit:boolean = false;
 }
 
-export const receptas: Array<Recepta> = [
-    {id: 1 , nombre: 'Gofres', 
-    pasos:'Lorem ipsum dolor sit amet consectetur adipiscing elit vestibulum justo litora sem, ullamcorper eleifend fermentum pulvinar mollis sed nullam sociis mi senectus. Est ut malesuada suspendisse sagittis fames feugiat morbi egestas fringilla litora, dictum vitae dignissim interdum netus laoreet cras magnis tellus. Mauris iaculis magnis porta sed porttitor varius etiam eros, laoreet condimentum conubia lobortis felis vel eget vivamus, sociis cursus cubilia curabitur nulla sem montes.', 
-    ingredients:'farina,sucre,ous', comentaris:[
-      {id:1, stars:4, review: 'Molt bona!', dia: new Date()},
-      {id:2, stars:2, review: 'No m\'agradat', dia: new Date()}
-    ]},
-    {id: 2 , nombre: 'Tortitas',
-    pasos:'Lorem ipsum dolor sit amet consectetur adipiscing elit vestibulum justo litora sem, ullamcorper eleifend fermentum pulvinar mollis sed nullam sociis mi senectus. Est ut malesuada suspendisse sagittis fames feugiat morbi egestas fringilla litora, dictum vitae dignissim interdum netus laoreet cras magnis tellus. Mauris iaculis magnis porta sed porttitor varius etiam eros, laoreet condimentum conubia lobortis felis vel eget vivamus, sociis cursus cubilia curabitur nulla sem montes.', 
-    ingredients:'farina,sucre,ous', comentaris:[
-      {id:1, stars:4, review: 'Molt bona!', dia: new Date()},
-      {id:2, stars:2, review: 'No m\'agradat', dia: new Date()}
-     ]},
-    {id: 3 , nombre: 'Pastel',
-    pasos:'Lorem ipsum dolor sit amet consectetur adipiscing elit vestibulum justo litora sem, ullamcorper eleifend fermentum pulvinar mollis sed nullam sociis mi senectus. Est ut malesuada suspendisse sagittis fames feugiat morbi egestas fringilla litora, dictum vitae dignissim interdum netus laoreet cras magnis tellus. Mauris iaculis magnis porta sed porttitor varius etiam eros, laoreet condimentum conubia lobortis felis vel eget vivamus, sociis cursus cubilia curabitur nulla sem montes.', 
-    ingredients:'farina,sucre,ous', comentaris:[
-      {id:1, stars:4, review: 'Molt bona!', dia: new Date()},
-      {id:2, stars:2, review: 'No m\'agradat', dia: new Date()}
-    ]}
-  ];
+export const receptas: Array<Recepta> = [];
+
+/*
+
+Antiga arraylist que tenia (part1)
+
+{id: 1 , nombre: 'Gofres', 
+pasos:'Lorem ipsum dolor sit amet consectetur adipiscing elit vestibulum justo litora sem, ullamcorper eleifend fermentum pulvinar mollis sed nullam sociis mi senectus. Est ut malesuada suspendisse sagittis fames feugiat morbi egestas fringilla litora, dictum vitae dignissim interdum netus laoreet cras magnis tellus. Mauris iaculis magnis porta sed porttitor varius etiam eros, laoreet condimentum conubia lobortis felis vel eget vivamus, sociis cursus cubilia curabitur nulla sem montes.', 
+ingredients:'farina,sucre,ous', comentaris:[
+  {id:1, stars:4, review: 'Molt bona!', dia: new Date()},
+  {id:2, stars:2, review: 'No m\'agradat', dia: new Date()}
+]},
+{id: 2 , nombre: 'Tortitas',
+pasos:'Lorem ipsum dolor sit amet consectetur adipiscing elit vestibulum justo litora sem, ullamcorper eleifend fermentum pulvinar mollis sed nullam sociis mi senectus. Est ut malesuada suspendisse sagittis fames feugiat morbi egestas fringilla litora, dictum vitae dignissim interdum netus laoreet cras magnis tellus. Mauris iaculis magnis porta sed porttitor varius etiam eros, laoreet condimentum conubia lobortis felis vel eget vivamus, sociis cursus cubilia curabitur nulla sem montes.', 
+ingredients:'farina,sucre,ous', comentaris:[
+  {id:1, stars:4, review: 'Molt bona!', dia: new Date()},
+  {id:2, stars:2, review: 'No m\'agradat', dia: new Date()}
+ ]},
+{id: 3 , nombre: 'Pastel',
+pasos:'Lorem ipsum dolor sit amet consectetur adipiscing elit vestibulum justo litora sem, ullamcorper eleifend fermentum pulvinar mollis sed nullam sociis mi senectus. Est ut malesuada suspendisse sagittis fames feugiat morbi egestas fringilla litora, dictum vitae dignissim interdum netus laoreet cras magnis tellus. Mauris iaculis magnis porta sed porttitor varius etiam eros, laoreet condimentum conubia lobortis felis vel eget vivamus, sociis cursus cubilia curabitur nulla sem montes.', 
+ingredients:'farina,sucre,ous', comentaris:[
+  {id:1, stars:4, review: 'Molt bona!', dia: new Date()},
+  {id:2, stars:2, review: 'No m\'agradat', dia: new Date()}
+]}
+*/
--- src/app/receptas/receptas.component.ts
+++ src/app/receptas/receptas.component.ts
@@ -4,6 +4,8 @@ import { Comentari } from '../modelos/comentari.modelo';
 import { Recepta } from '../modelos/recepta.modelo';
 import { receptas } from '../modelos/recepta.modelo';
 
+import axios from 'axios';
+
 @Component({
   selector: 'app-receptas',
   templateUrl: './receptas.component.html',
@@ -13,11 +15,37 @@ export class ReceptasComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-  
   receptasComp=receptas;
+
+  ngOnInit(): void {
+    axios.get('http://localhost:3000/receptes')
+            .then(resp => {
+                var data = resp.data;
+                data.forEach(e => {
+                  var receptaAPI:Recepta = new Recepta();
+                  receptaAPI.id=e.id;
+                  receptaAPI.nombre=e.nom;
+                  receptaAPI.pasos=e.elaboracio;
+                  receptaAPI.ingredients=e.ingredients;
+                  e.reviews.forEach(c =>{
+                    var comentariAPI:Comentari = new Comentari();
+                    comentariAPI.id=c.id;
+                    comentariAPI.stars=c.stars;
+                    comentariAPI.review=c.body;
+                    comentariAPI.dia=c.createdOn;
+                    receptaAPI.comentaris.push(comentariAPI);
+                  })
+                  this.receptasComp.push(receptaAPI);
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+  }
+
   receptasForm:Recepta = new Recepta();
+
+  receptasEdit:Recepta = new Recepta();
 
   comentari:Comentari = new Comentari();
 
@@ -50,20 +78,24 @@ export class ReceptasComponent implements OnInit {
   //(gracies a ella utilitzaré l'ultim id sempre alhora de crear una nova recepta)
   ultimIDRecepta(){
     var i:any = 0;
-    for (i in receptas) {
-      console.log(i);
+    for (i in this.receptasComp) {
+      console.log(this.receptasComp[i].nombre);
     }
-    return i;
+    var n:number = Number(this.receptasComp[i].id);
+    n=n+1;
+    return n;
   }
 
   //Funció amb la que extrec el ultim id de comentaris
   //(gracies a ella utilitzaré l'ultim id sempre alhora de crear un nou comentari)
   ultimIDComentari(idRecepta:number){
     var i:any = 0;
-    for (i in receptas[idRecepta].comentaris) {
+    for (i in this.receptasComp[idRecepta-1].comentaris) {
       console.log(i);
     }
-    return i;
+    var n:number = Number(this.receptasComp[idRecepta-1].comentaris[i].id);
+    n=n+1;
+    return n;
   }
 
   //Aquesta funció comprova si el nom de la recepta que es vol crear ja existeix, 
@@ -80,28 +112,119 @@ export class ReceptasComponent implements OnInit {
 
   //Funció per afegir una recepta 
   addRecepta(){
+    /*
+    ANTIC CODI DE LA PART 1
+
     let validar:boolean = this.comprovarNomRecepta(this.receptasForm.nombre);
     if(validar==true){
       this.receptasForm.id = this.ultimIDRecepta()+1;
       this.receptasComp.push(this.receptasForm);
     }
     this.receptasForm = new Recepta();
+    */
+   
+    let validar:boolean = this.comprovarNomRecepta(this.receptasForm.nombre);
+    if(validar==true){
+      this.receptasForm.id = this.ultimIDRecepta();
+      this.receptasComp.push(this.receptasForm);
+      console.log(this.receptasForm.id);
+    }
+
+    axios.post('http://localhost:3000/receptes', {
+      nom: this.receptasForm.nombre,
+      elaboracio: this.receptasForm.pasos,
+      ingredients: this.receptasForm.ingredients,
+      reviews: this.receptasForm.comentaris,
+      id: this.receptasForm.id
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    this.receptasForm = new Recepta();
+
   }
 
   //Funcio per afegir un comentari en l'array de comentaris d'una recepta
   addComentari(id:number){
     var i:any = 0;
-    for (i in receptas) {
-      if(receptas[i].id==id){
-        console.log('Lo he encontrado '+receptas[i].nombre);
+    for (i in this.receptasComp) {
+      if(this.receptasComp[i].id==id){
+        console.log('Lo he encontrado '+this.receptasComp[i].nombre);
         if(this.comentari.stars!=null && this.comentari.review!=""){
             this.comentari.id = this.ultimIDComentari(id)+1;
             this.comentari.dia = new Date();
-            receptas[i].comentaris.push(this.comentari);
+            this.receptasComp[i].comentaris.push(this.comentari);
         }
         this.comentari = new Comentari();
       }
     }
+
+    axios.put('http://localhost:3000/receptes/'+id, {
+      nom: this.receptasComp[id-1].nombre,
+      elaboracio: this.receptasComp[id-1].pasos,
+      ingredients: this.receptasComp[id-1].ingredients,
+      reviews: this.receptasComp[id-1].comentaris,
+      id: id
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
   }
+
+  
+  //Funció per editar una recepta
+  canEdit(id:number){
+    if(this.receptasComp[id-1].edit==false){
+      this.receptasComp[id-1].edit=true;
+    }else if(this.receptasComp[id-1].edit==true){
+      this.receptasComp[id-1].edit=false;
+    }
+  }
+
+  //Funció per editar una recepta
+  editar(id:number){
+    this.receptasComp[id-1].ingredients=this.receptasEdit.ingredients;
+    this.receptasComp[id-1].pasos=this.receptasEdit.pasos;
+    this.receptasEdit = new Recepta();
+
+    axios.put('http://localhost:3000/receptes/'+id, {
+      nom: this.receptasComp[id-1].nombre,
+      elaboracio: this.receptasComp[id-1].pasos,
+      ingredients: this.receptasComp[id-1].ingredients,
+      reviews: this.receptasComp[id-1].comentaris,
+      id: id
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    this.receptasComp[id-1].edit=false;
+
+  }
+
+  //Funció per eliminar una recepta
+  eliminar(id:number){
+    this.receptasComp.splice(id-1, 1);
+    axios.delete('http://localhost:3000/receptes/'+id, {
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
 
 }
